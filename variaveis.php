9c99c45da863da5c512cbc73201e8ab0fc5e85a2
Marcos Aleixo <maknytz@gmail.com>
2025-11-18 16:17:54 -0300

    Criando variaveis no PHP

---
+++ variaveis.php
@@ -0,0 +1,13 @@
+<?php
+
+$valorDia = 80;
+$gorjetas = 40;
+
+$diaria = $valorDia + $gorjetas;
+
+echo "Segunda-feira: $diaria\n";
+
+$gorjetas = 15;
+
+$diaria = $valorDia + $gorjetas;
+echo "Terça-feira: $diaria";
