815111a3a90ac03e91ddb3e0660bb10097c8e65b
Marcos Aleixo <maknytz@gmail.com>
2025-11-18 16:31:54 -0300

    Trabalhando com strings no PHP

---
+++ string.php
@@ -0,0 +1,11 @@
+<?php
+
+$curso = "Curso de PHP";
+$valorDia = "80";
+$gorjetas = '15';
+$soma = $valorDia + $gorjetas;
+$texto = "<h1>Título 1</h1>";
+
+echo $curso."\n";
+echo $soma."\n";
+echo $texto;
