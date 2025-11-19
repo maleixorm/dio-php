642a5bfc392ceae94b55acef3121c33e48ff833d
Marcos Aleixo <maknytz@gmail.com>
2025-11-19 17:00:03 -0300

    Switch case no PHP

---
+++ switchcase.php
@@ -0,0 +1,23 @@
+<?php
+
+$sorteio = rand(0,5);
+
+echo "Valor sorteado: $sorteio\n";
+
+switch ($sorteio) {
+    case 0:
+        echo "Você ganhou 2 pontos!";
+        break;
+    
+    case 1:
+        echo "Você perdeu 1 ponto!";
+        break;
+    
+    case 2:
+        echo "Você ganhou um bônus: Parabéns! Você ganhou 3 pontos!";
+        break;
+    
+    default:
+        echo "Jogue novamente!";
+        break;
+}
