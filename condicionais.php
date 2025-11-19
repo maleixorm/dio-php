bd8db40eff80aff3deafe78d01607047ec4cd9de
Marcos Aleixo <maknytz@gmail.com>
2025-11-19 16:32:06 -0300

    Utilizando o if/else e operador ternário

--- condicionais.php
+++ condicionais.php
@@ -9,5 +9,16 @@ if ($variavel1 == $variavel2 && 10 > 5) {
     if (8 == 4) {
         echo "Verdadeiro";
     }
-    echo "Variáveis são iguais ou 10 é menor do que 5";
+    echo "Variáveis são iguais ou 10 é menor do que 5\n";
 }
+
+$nota = 6;
+
+if ($nota >= 7) {
+    echo "Aluno aprovado.\n";
+} else {
+    echo "Aluno reprovado.\n";
+}
+
+$nota2 = 8;
+echo $nota2 >= 7 ? "Aluno Aprovado.\n" : "Aluno Reprovado.\n";
