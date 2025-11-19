c1ade39d831386df64aae487b04158516e8c20bb
Marcos Aleixo <maknytz@gmail.com>
2025-11-19 16:14:24 -0300

    Trabalhando com condicionais no PHP

---
+++ condicionais.php
@@ -0,0 +1,13 @@
+<?php
+
+$variavel1 = true;
+$variavel2 = true;
+$variavel3 = true;
+
+if ($variavel1 == $variavel2 && 10 > 5) {
+    echo "Primeiro IF\n";
+    if (8 == 4) {
+        echo "Verdadeiro";
+    }
+    echo "Variáveis são iguais ou 10 é menor do que 5";
+}
