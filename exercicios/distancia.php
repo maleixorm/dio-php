2821deaa1aaac7439a030198e550600ad81b9097
Marcos Aleixo <maknytz@gmail.com>
2025-11-20 00:01:32 -0300

    Desafio distância

---
+++ exercicios/distancia.php
@@ -0,0 +1,12 @@
+<?php
+  // Leia o Desafio e siga o código base abaixo para resolvê-lo. Algumas dicas:
+  // 1. Use a função "fgets(STDIN)" para ler uma linha de Entrada;
+  // 2. Use a função "echo" formatar e imprimir sua(s) Saída(s).
+
+  $distancia = fgets(STDIN);
+
+  //TODO: Calcule e imprima o tempo necessário de acordo com o enunciado.
+  $result = $distancia * 2;
+  
+  echo "$result minutos";
+?>
